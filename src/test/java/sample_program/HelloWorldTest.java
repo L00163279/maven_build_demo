595b08b15875a10ecfddf838ba283137887b6d18
MShafeeq <L00163279@student.lyit.ie>
2021-11-05 10:27:46 +0000

    updated code

--- src/test/java/sample_program/HelloWorldTest.java
+++ src/test/java/sample_program/HelloWorldTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class HelloWorldTest {
 	@Test
 	public void testGreetingMessage() {
-		assertEquals("Hello John! Welcome to Java Programming", HelloWorld.getGreetingMessage("John"));
+		assertEquals("Hello MrJohn! Welcome to Java Programming", HelloWorld.getGreetingMessage("John"));
 	}
 }
